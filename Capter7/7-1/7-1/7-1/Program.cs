52d0206642718b8ceb9ae82c9a7f8a90b7f0f8b2
Nanishi Takana <shionorzero@gmail.com>
2020-07-08 01:14:29 +0900

    7/8

---
+++ Capter7/7-1/7-1/7-1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book
+            {
+                Title = "吾輩 は 猫 で ある",
+                Author = "夏目漱石",
+                Pages = 610,
+                Rating = 4,
+                Memo = "書き出しが面白い"
+            };
+
+            book.print();
+        }
+
+        class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public int Pages { get; set; }
+            public int Rating { get; set; }
+            public string Memo { get; set; }
+
+            public void print()
+            {
+                Console.WriteLine($"タイトル：{Title}");
+                Console.WriteLine($"著者：{Author}");
+                Console.WriteLine($"ページ数：{Pages}");
+                Console.WriteLine($"評価：{Rating}");
+                Console.WriteLine($"メモ：{Memo}");
+            }
+
+        }
+
+    }
+}
---
+++ Capter7/7-2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emploee1 = new Employee
+            {
+                Id = 1,
+                FamilyName = "正山" ,
+                GivenName = "周那",
+                EmailAddress = "email.com"               
+
+            };
+
+            Employee emploee2 = new Employee
+            {
+                Id = 2,
+                FamilyName = "宮本",
+                GivenName = "むさし",
+                EmailAddress = "email.com"
+
+            };
+
+            //フルネームの表示
+            Console.WriteLine(emploee1.GetFullName());
+            Console.WriteLine(emploee2.GetFullName());
+            
+        }
+
+        //従業員クラス
+        class Employee
+        {
+            public int Id { get; set; }
+            public string FamilyName { get; set; }
+            public string GivenName  { get; set; }
+            public string EmailAddress { get; set; }
+
+            public string GetFullName()
+            {
+               return FamilyName + GivenName;
+            }
+
+
+        }
+
+
+    }
+}
