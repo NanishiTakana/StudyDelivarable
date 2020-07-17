cc7696480fd21137434d538bf151aa45eede0035
Nanishi Takana <shionorzero@gmail.com>
2020-07-18 00:58:28 +0900

    7/17

---
+++ Chapter11/11-3/11-3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _11_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            var king = new Card(CardSuit.Spade, 13);
+            Console.WriteLine($"Suit:{king.Suit}, Number:{king.Number}");
+            ChangeToAce(king);
+            Console.WriteLine($"Suit:{king.Suit}, Number:{king.Number}");
+            
+        }
+
+        private static void ChangeToAce(Card card)
+        {
+            card = new Card(CardSuit.Club, 1);
+            card.Number = 1;
+            card.Suit = CardSuit.Heart;
+        }
+    }
+
+    enum CardSuit
+    {
+        Club, Spade, Heart, Diamond
+    }
+
+    class Card
+    {
+       
+        public CardSuit Suit { get; set; }
+        public int Number { get; set; }
+
+        public Card(CardSuit suit, int number)
+        {
+            Suit = suit;
+            Number = number;
+        }
+    }
+}
---
+++ Chapter12/12-1/12-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dates = new List<DateTime>()
+            {
+                new DateTime(2019,11,21,12,22,00),
+                new DateTime(3221,12,21,13,54,44)
+            };
+
+            Console.WriteLine($"リストオブジェクトに格納されているDateTimeオブジェクトの数は{dates.Count}");
+            foreach (DateTime n in dates)
+            {
+                Console.WriteLine(n.ToString("yyyy年mm月dd日 HH:mm"));
+                
+            }
+ 
+
+        }
+    }
+}
