cd48d73294f56758b17b9c195f457a558165a0c6
Nanishi Takana <shionorzero@gmail.com>
2020-07-24 02:37:02 +0900

    7/23

---
+++ Chapter15/15-1/15-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _15_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[5]
+                {
+                    1,2,2,2,4
+                };
+
+            try
+            {
+                Console.WriteLine(array[5]);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine( $"Type : {ex.GetType().Name}");
+                Console.WriteLine($"Type : {ex.Message}");
+            }
+
+
+
+        }
+    }
+}
