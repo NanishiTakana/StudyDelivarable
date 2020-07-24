09d88a3245646d7717c25bafddea3a7d40e1144e
Nanishi Takana <shionorzero@gmail.com>
2020-07-25 00:54:48 +0900

    7/24

---
+++ Chapter15/15-2/15-2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _15_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+
+                var total = 1000;
+                var count = 0;
+
+                Console.WriteLine("割る数を入力してください");
+                var line = Console.ReadLine();
+                if (int.TryParse(line, out var num))
+                {
+                    count = num;
+                    //countが0じゃないとき割り算をする
+                    if (count != 0)
+                    {
+                        var ans = total / count;
+                        Console.WriteLine(ans);
+                        Console.WriteLine("正常終了");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("0で割ることはできません。もう一度入力してください");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("入力された値が正しくありません。もう一度入力してください");
+                }
+
+
+            }
+
+        }
+    }
+}
---
+++ Chapter15/15－3/15－3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _15_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WriteSample();
+            }
+            catch
+            {
+                Console.WriteLine("エラーが発生しました");
+            }
+        }
+
+        private static void WriteSample()
+        {
+            using (var sw = new StreamWriter("sample.txt"))
+            {
+                sw.WriteLine(" 吾輩は猫である。");
+                sw.WriteLine(" 名前はまだ無い");
+                sw.WriteLine(" どこで生れ たかとんと見当がつかぬ。");
+                Console.WriteLine("書き込み完了");
+            }
+        }
+    }
+
+}
