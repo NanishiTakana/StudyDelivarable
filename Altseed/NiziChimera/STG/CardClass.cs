9db3685073ab0dff76c770eddf0704e7ee2190bb
Nanishi Takana <shionorzero@gmail.com>
2020-09-02 03:15:30 +0900

    9/1

--- Altseed/NiziChimera/STG/CardClass.cs
+++ Altseed/NiziChimera/STG/CardClass.cs
@@ -1,52 +1,79 @@
-﻿using System;
+﻿using asd;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace STG
 {
-    class CardClass
+    class CardClass : TextureObject2D
     {
         //カード番号
-        public string CardNumber { get; set; }
+        public string cardNumber;
         //ライバー名
-        public string  FULLName { get; set; }
-        public string SplitName { get; set; }
+        public string fULLName;
+        public string splitName ;
         //カードの属性
-        public string CardType { get; set; }
+        public string cardType;
         //カード説明
-        public string CardExplanation { get; set; }
-
-
+        public string cardExplanation;
 
         //戦闘力パラメータ
-        public int BattleParameter { get; set; }
+        public int battleParameter;
         //狂気パラメータ
-        public int InsanityParameter { get;set;}
+        public int insanityParameter;
         //頭脳パラメータ
-        public int BrainParameter { get; set; }
+        public int brainParameter;
 
+        //読み込んだカード情報の1行分を保持する
+        string inputLine = "";
+        //読み込んだカード情報の各値を格納する変数
+        string[] inputInfoList;
+        //読み込んだ頭カード情報の行分を格納する変数
+        public List<string[]> inputLineList = new List<string[]>();
 
-        public CardClass( string cardNumber,  string fullName, string splitName, 
-                         string cardType,
-                         string cardExplanation,
-                         int battleParameter,
-                         int InsanityParameter,
-                         int brainParameter)
+        //一覧からカードを読み込む
+        public CardClass(string cardType )
         {
-            this.CardNumber = cardNumber;
-            this.FULLName = fullName;
-            this.SplitName = splitName;
-            this.CardType = cardType;
-            this.CardExplanation = cardExplanation;
+          
 
-            this.BattleParameter = battleParameter;
-            this.InsanityParameter = InsanityParameter;
-            this.BrainParameter = battleParameter;
+            if (cardType == "head")
+            {
+                getCardInfo("head");
+
+            }
+            else if (cardType == "body")
+            {
+                getCardInfo("body");
+            }
+            else if (cardType == "lower")
+            {
+                getCardInfo("lower");
+            }
 
         }
 
+        public void getCardInfo(string getType)
+        {
+            using (StreamReader sr = new StreamReader($"Resources/{getType}.txt", Encoding.GetEncoding("UTF-8")))
+            {
+
+                while ((inputLine = sr.ReadLine()) != null)
+                {
+                    inputInfoList = inputLine.Split(',');
+                    inputLineList.Add(inputInfoList);
+                }
+            }
+
+        }
+
+        public string getCardComponent(int cardNo,int factorNo)
+        {
+            return inputLineList[cardNo][factorNo];
+
+        }
 
 
 
--- Altseed/NiziChimera/STG/Scene/CustamModeScene.cs
+++ Altseed/NiziChimera/STG/Scene/CustamModeScene.cs
@@ -14,67 +14,76 @@ namespace STG
         asd.Layer2D headLayer;
         //体を表示するレイヤーを持つ
         asd.Layer2D bodyLayer;
-        //体を表示するレイヤーを持つ
+        //足を表示するレイヤーを持つ
         asd.Layer2D lowerLayer;
 
-        //読み込んだカード情報の1行分を保持する
-        string inputLine = "";
-        //カード情報の要素数
-        int amoCardInfo = 8;
-
-        //読み込んだカード情報の詳細を格納する変数
-        string[] inputInfoList ;
-
-        //読み込んだ頭カード情報の行分を格納する変数
-        List<string[]> inputLineList = new List<string[]>() ;
-
-        Random randomHead = new System.Random();
-
-        //静的？
-        //string headCardNum = randomHead.Next(1, 4).ToString("000");
-
+        Random randomNum = new System.Random();
 
         protected override void OnRegistered()
         {
             // 2Dを表示するレイヤーのインスタンスを生成する。
             headLayer = new asd.Layer2D();
+            bodyLayer = new asd.Layer2D();
+            lowerLayer = new asd.Layer2D();
 
             // シーンにレイヤーのインスタンスを追加する。
+            AddLayer(lowerLayer);
+            AddLayer(bodyLayer);
             AddLayer(headLayer);
 
-            inputInfoList = new string[amoCardInfo];
-            GetCardInfo(inputInfoList,"head");
-            GetCardInfo(inputInfoList, "body");
-            GetCardInfo(inputInfoList, "lower");
+            //表示基準線
+            int xLine = 400;
+            int yLine = 200;
 
-            Console.WriteLine(inputLineList[1][0]);
+            //頭
+            CardClass headCard = new CardClass("head");
+            Console.WriteLine(headCard.getCardComponent(1, 1));
 
-        }
+            int headCardNum = randomNum.Next(1, 4);
 
-        private string[] GetCardInfo(string[] infoList,string getType)
-        {
-            using (StreamReader sr = new StreamReader($"Resources/{getType}.txt", Encoding.GetEncoding("UTF-8")))
+            headCard.Texture = asd.Engine.Graphics.CreateTexture2D($"Resources/head/head{headCardNum.ToString("000")}.png");
+
+            int centerY;
+            if (int.TryParse(headCard.getCardComponent(headCardNum - 1, 9), out centerY))
             {
-
-                while ((inputLine = sr.ReadLine()) != null)
-                {
-                    infoList = inputLine.Split(',');
-                    inputLineList.Add(infoList);
-                }
+                headCard.CenterPosition = new asd.Vector2DF(headCard.Texture.Size.X / 2.0f, centerY);
             }
+            else
+            {
+                Console.WriteLine("数値に変換できません");
+            }
+            headCard.Position = new asd.Vector2DF(xLine, yLine);
+            headCard.Scale = new asd.Vector2DF(0.5f, 0.5f);
 
-            string CardNum = randomHead.Next(1, 4).ToString("000");
 
-            asd.TextureObject2D headImage = new asd.TextureObject2D();
-            headImage.Texture = asd.Engine.Graphics.CreateTexture2D($"Resources/{getType}/{getType}{CardNum}.png");
-            Console.WriteLine($"Resources/{getType}/{getType}{CardNum}.png");
-            headImage.Position = new asd.Vector2DF(320, 240);
-            headImage.Scale = new asd.Vector2DF(0.5f, 0.5f);
+            //体
+            CardClass bodyCard = new CardClass("body");
+            Console.WriteLine(bodyCard.getCardComponent(1, 1));
+
+            string bodyCardNum = randomNum.Next(1, 4).ToString("000");
+            bodyCard.Texture = asd.Engine.Graphics.CreateTexture2D($"Resources/body/body{bodyCardNum}.png");
+            bodyCard.Position = new asd.Vector2DF(490, 200);
+            bodyCard.Scale = new asd.Vector2DF(0.5f, 0.5f);
+
+            //足
+            CardClass lowerCard = new CardClass("lower");
+            Console.WriteLine(bodyCard.getCardComponent(1, 1));
+
+            string lowerCardNum = randomNum.Next(1, 4).ToString("000");
+            lowerCard.Texture = asd.Engine.Graphics.CreateTexture2D($"Resources/lower/lower{lowerCardNum}.png");
+            lowerCard.Position = new asd.Vector2DF(490, 400);
+            lowerCard.Scale = new asd.Vector2DF(0.5f, 0.5f);
+
+
 
             // レイヤーにオブジェクトのインスタンスを追加する。
-            headLayer.AddObject(headImage);
-            return infoList;
+            headLayer.AddObject(headCard);
+            bodyLayer.AddObject(bodyCard);
+            lowerLayer.AddObject(lowerCard);
+
+
         }
+
 
     }
 
