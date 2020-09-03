6403d524f315400c3b0336da834ec532c0b8bc39
Nanishi Takana <shionorzero@gmail.com>
2020-09-04 01:45:06 +0900

    9/3

--- Altseed/NiziChimera/STG/Scene/CustamModeScene.cs
+++ Altseed/NiziChimera/STG/Scene/CustamModeScene.cs
@@ -31,50 +31,69 @@ namespace STG
             AddLayer(bodyLayer);
             AddLayer(headLayer);
 
-            //表示基準線
-            int xLine = 400;
-            int yLine = 200;
+            //表示基準座標
+            int xLine = 300;
+            int yLine = 150;
+
+            //幅比率
+            float bodyRatio;
+            float lowerRatio;
+
 
             //頭
             CardClass headCard = new CardClass("head");
-            Console.WriteLine(headCard.getCardComponent(1, 1));
 
             int headCardNum = randomNum.Next(1, 4);
-
+            //画像の読み込み
             headCard.Texture = asd.Engine.Graphics.CreateTexture2D($"Resources/head/head{headCardNum.ToString("000")}.png");
 
-            int centerY;
-            if (int.TryParse(headCard.getCardComponent(headCardNum - 1, 9), out centerY))
-            {
-                headCard.CenterPosition = new asd.Vector2DF(headCard.Texture.Size.X / 2.0f, centerY);
-            }
-            else
-            {
-                Console.WriteLine("数値に変換できません");
-            }
+            Console.WriteLine($"headcardnum:{headCardNum}");
+            //画像の中心点のY座標をデータファイルから取得
+            int headCenterY = IntParse(headCard.getCardComponent(headCardNum - 1, 9));
+            headCard.CenterPosition = new asd.Vector2DF(headCard.Texture.Size.X / 2.0f, headCenterY);
+
             headCard.Position = new asd.Vector2DF(xLine, yLine);
             headCard.Scale = new asd.Vector2DF(0.5f, 0.5f);
 
 
             //体
             CardClass bodyCard = new CardClass("body");
-            Console.WriteLine(bodyCard.getCardComponent(1, 1));
 
-            string bodyCardNum = randomNum.Next(1, 4).ToString("000");
-            bodyCard.Texture = asd.Engine.Graphics.CreateTexture2D($"Resources/body/body{bodyCardNum}.png");
-            bodyCard.Position = new asd.Vector2DF(490, 200);
-            bodyCard.Scale = new asd.Vector2DF(0.5f, 0.5f);
+            int bodyCardNum = randomNum.Next(1, 4);
+            Console.WriteLine($"bodycardnum:{bodyCardNum}");
+            //画像の読み込み
+            bodyCard.Texture = asd.Engine.Graphics.CreateTexture2D($"Resources/body/body{bodyCardNum.ToString("000")}.png");
+
+            //画像の中心点のY座標をデータファイルから取得
+            int bodyCenterY = IntParse(bodyCard.getCardComponent(bodyCardNum - 1, 9));
+            bodyCard.CenterPosition = new asd.Vector2DF(bodyCard.Texture.Size.X / 2.0f, bodyCenterY);
+
+            //頭と同じ座標に表示
+            bodyCard.Position = headCard.Position;
+            Console.WriteLine("test");
+            //頭と体の大きさの比率を計算
+            bodyRatio = (float)IntParse(headCard.getCardComponent(headCardNum - 1, 8)) / 
+                        IntParse(bodyCard.getCardComponent(bodyCardNum - 1, 8)) ;
+
+            Console.WriteLine("test2");
+
+            bodyCard.Scale = new asd.Vector2DF(bodyRatio * 0.5f, bodyRatio * 0.5f);
 
             //足
             CardClass lowerCard = new CardClass("lower");
-            Console.WriteLine(bodyCard.getCardComponent(1, 1));
 
-            string lowerCardNum = randomNum.Next(1, 4).ToString("000");
-            lowerCard.Texture = asd.Engine.Graphics.CreateTexture2D($"Resources/lower/lower{lowerCardNum}.png");
-            lowerCard.Position = new asd.Vector2DF(490, 400);
+            int lowerCardNum =  randomNum.Next(1, 4);
+            Console.WriteLine($"lowerCardNum{lowerCardNum}");
+            lowerCard.Texture = asd.Engine.Graphics.CreateTexture2D($"Resources/lower/lower{lowerCardNum.ToString("000")}.png");
+
+
+            int lowerCenterY = IntParse(lowerCard.getCardComponent(lowerCardNum - 1, 9));
+            lowerCard.CenterPosition = new asd.Vector2DF(lowerCard.Texture.Size.X / 2.0f, lowerCenterY);
+
+            int bodyUnionY = IntParse(bodyCard.getCardComponent(bodyCardNum - 1, 10));
+            lowerCard.Position = new asd.Vector2DF(xLine, bodyUnionY / 2.0f + bodyCard.Position.Y);
+
             lowerCard.Scale = new asd.Vector2DF(0.5f, 0.5f);
-
-
 
             // レイヤーにオブジェクトのインスタンスを追加する。
             headLayer.AddObject(headCard);
@@ -83,6 +102,25 @@ namespace STG
 
 
         }
+
+        //String => Int変換メソッド
+        public int IntParse(string str)
+        {
+            int num = 0;
+            if (int.TryParse(str, out num))
+            {
+                Console.WriteLine($"変換成功{num}");
+            }
+            else
+            {
+                Console.WriteLine("数値に変換できません");
+            }
+            return num;
+
+        }
+
+
+        
 
 
     }
